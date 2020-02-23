8890a86885a1135abc2bf415d782198dccfcef1a
Nick Maltbie <nick.dmalt@gmail.com>
2020-02-23 04:36:59 -0500

    disabled cors

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.secret_key = os.urandom(24)
 cred = credentials.Certificate('secrets/firebase-key.json')
 default_app = initialize_app(cred)
 
-CORS(app)
+#CORS(app)
 
 app.register_blueprint(rooms_api, url_prefix='/rooms')
 app.register_blueprint(games_api, url_prefix='/games')
